93668c08b2efa978e4aa793770033ac4be459813
Ayobami6 <ayobamidele006@gmail.com>
2024-05-18 10:02:00 +0100

    wrap logging in try and except block

--- restaurant_go/settings.py
+++ restaurant_go/settings.py
@@ -172,47 +172,52 @@ logger = logging.getLogger(__name__)
 
 LOG_LEVEL = "INFO"
 
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "console": {
-                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+
+try:
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                },
+                "file": {
+                    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                },
+                "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
             },
-            "file": {
-                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            "handlers": {
+                "console": {
+                    "level": LOG_LEVEL,
+                    "class": "logging.StreamHandler",
+                    "formatter": "console",
+                },
+                "file": {
+                    "level": LOG_LEVEL,
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "formatter": "file",
+                    "filename": BASE_DIR / "logs" / "django.log",
+                    "maxBytes": 1024 * 1024 * 10,  # 5 MB
+                    "backupCount": 5,
+                },
+                "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
             },
-            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-        },
-        "handlers": {
-            "console": {
-                "level": LOG_LEVEL,
-                "class": "logging.StreamHandler",
-                "formatter": "console",
+            "loggers": {
+                "": {
+                    "level": LOG_LEVEL,
+                    "handlers": ["console", "file"],
+                    "propagate": False,
+                },
+                "apps": {
+                    "level": LOG_LEVEL,
+                    "handlers": ["console", "file"],
+                    "propagate": False,
+                },
             },
-            "file": {
-                "level": LOG_LEVEL,
-                "class": "logging.handlers.RotatingFileHandler",
-                "formatter": "file",
-                "filename": BASE_DIR / "logs" / "django.log",
-                "maxBytes": 1024 * 1024 * 10,  # 5 MB
-                "backupCount": 5,
-            },
-            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-        },
-        "loggers": {
-            "": {
-                "level": LOG_LEVEL,
-                "handlers": ["console", "file"],
-                "propagate": False,
-            },
-            "apps": {
-                "level": LOG_LEVEL,
-                "handlers": ["console", "file"],
-                "propagate": False,
-            },
-        },
-        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-    }
-)
+            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+        }
+    )
+
+except Exception:
+    pass
