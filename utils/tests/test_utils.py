219d172d8b0088349839dab7a41faec81a2f12ce
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-05-26 16:27:57 +0100

    Added few testcases to test the send_reset_otp method

--- utils/tests/test_utils.py
+++ utils/tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import patch, MagicMock
-from utils.utils import send_otp
+from utils.utils import send_otp, send_reset_otp
 
 
 @pytest.mark.parametrize(
@@ -55,3 +55,38 @@ def test_send_otp_exception(email, username):
         otp = send_otp(email, username)
 
         assert otp is None
+
+
+@pytest.mark.parametrize(
+    "email",
+    [
+        "test@example.com"
+    ]
+)
+def test_send_reset_otp(email):
+    with patch("utils.utils.sendmail") as mocked_sendmail:
+        otp = send_reset_otp(email)
+
+        assert otp is not None
+        assert len(otp) == 6
+        message = f"""
+        Your One Time Reset Password is {otp}
+        <br />
+        Expires in 10 minutes
+        """
+        mocked_sendmail.assert_called_once_with(
+            subject="Password Reset Code",
+            message=message,
+            user_email=email,
+        )
+
+
+@pytest.mark.parametrize(
+    "email",
+    [
+        ""
+    ]
+)
+def test_send_reset_otp_missing_input(email):
+    with pytest.raises(ValueError):
+        send_reset_otp(email)
