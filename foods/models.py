ade099acf84e431eea313aca3a92fc1cc0ce8da2
Ayobami Alaran A <ayobamidele006@gmail.com>
2024-06-25 12:31:58 +0100

    Add new food model&update foodpackage model with total_purchase count

--- foods/models.py
+++ foods/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from utils.decorators import str_meta
 
 
 # Create your models here.
+
+
+@str_meta
+class FoodCategory(models.Model):
+    name = models.CharField(max_length=100)
 
 
 class FoodPackage(models.Model):
@@ -11,9 +17,19 @@ class FoodPackage(models.Model):
     description = models.TextField(
         null=True, blank=True, help_text=_("Package Description")
     )
+    category = models.ForeignKey(
+        FoodCategory,
+        on_delete=models.SET_NULL,
+        related_name="category",
+        blank=True,
+        null=True,
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
     available_quantity = models.IntegerField(default=0)
+    total_purchase = models.BigIntegerField(default=0)
     date_created = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(default=timezone.now)
 
@@ -23,6 +39,17 @@ class FoodPackage(models.Model):
     class Meta:
         verbose_name = "Food Package"
         verbose_name_plural = "Food Packages"
+
+    def reduce_quantity(self) -> None:
+        """This is a util method to reduce the quantity"""
+        if self.available_quantity > 0:
+            self.available_quantity -= 1
+            self.save()
+
+    def increase_total_purchase(self) -> None:
+        """This is a util method to increase the the total purchase"""
+        self.total_purchase += 1
+        self.save()
 
 
 class FoodAsset(models.Model):
@@ -62,3 +89,37 @@ class FoodItem(models.Model):
     class Meta:
         verbose_name = "Food Item"
         verbose_name_plural = "Food Items"
+
+
+@str_meta
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(
+        null=True, blank=True, help_text=_("Package Description")
+    )
+    category = models.ForeignKey(
+        FoodCategory,
+        on_delete=models.SET_NULL,
+        related_name="food_category",
+        blank=True,
+        null=True,
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
+    available_quantity = models.IntegerField(default=0)
+    total_purchase = models.BigIntegerField(default=0)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
+
+    def reduce_quantity(self):
+        """This is a util method to reduce the quantity"""
+        if self.available_quantity > 0:
+            self.available_quantity -= 1
+            self.save()
+
+    def increase_total_purchase(self) -> None:
+        """This is a util method to increase the the total purchase"""
+        self.total_purchase += 1
+        self.save()
