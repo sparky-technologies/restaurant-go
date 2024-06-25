4c8cb75745f4a80dc9c79aef433c0cb8039fa321
Ayobami Alaran A <ayobamidele006@gmail.com>
2024-06-25 12:39:21 +0100

    add food and Food category to django admin

--- foods/admin.py
+++ foods/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from foods.models import FoodAsset, FoodItem, FoodPackage
+from foods.models import FoodAsset, FoodItem, FoodPackage, FoodCategory, Food
 
 # Register your models here.
 
@@ -17,6 +17,21 @@ class FoodItemStackedAdmin(admin.StackedInline):
 
 class FoodPackageAdmin(admin.ModelAdmin):
     inlines = [FoodAssetAdmin, FoodItemStackedAdmin]
+    list_display = ["name", "category", "price", "available_quantity", "total_purchase"]
+    list_filter = ["category"]
+    search_fields = [
+        "name",
+    ]
+
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ["name", "category", "price", "available_quantity", "total_purchase"]
+    list_filter = ["category"]
+    search_fields = [
+        "name",
+    ]
 
 
 admin.site.register(FoodPackage, FoodPackageAdmin)
+admin.site.register(FoodCategory)
+admin.site.register(Food, FoodAdmin)
